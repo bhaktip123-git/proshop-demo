2474c2da778e34bcdc28b74334831d10ff059e40
bhaktip123-git <bhakti.p@somaiya.edu>
2026-05-27 09:11:34 +0530

    updated message

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -1,6 +1,22 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Homescreen from './screens/Homescreen';
+
 const App = () => {
   return (
-    <h1>Welcome to Proshop</h1>
+    <>
+        <Header />
+        <main className='py-3'>
+          <Container>
+
+            <h1>Welcome to Proshop</h1>
+
+              <Homescreen />
+          </Container>
+       </main>
+       <Footer /> 
+    </>
   )
 }
 
