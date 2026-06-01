5f0bdafa5b16c6723760f9521d40b77d730c4ebf
bhaktip123-git <bhakti.p@somaiya.edu>
2026-06-01 13:46:56 +0530

    updated message

--- backend/models/productmodel.js
+++ backend/models/productmodel.js
@@ -1,10 +1,30 @@
-import mongoose
+import mongoose from 'mongoose';
+
+const reviewSchema = new mongoose.Schema({
+        user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true, 
+    ref: 'User'
+  },
+  name: {
+    type: String,   
+    required: true
+    },
+    rating: {   
+    type: Number,
+    required: true
+    },          
+    comment: {
+    type: String,
+    required: true
+    },
+}, {timestamps: true}); 
 
 const productSchema = new mongoose.Schema({
     user: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
-    ref: 'User'
+    ref: 'User',
   },  
   name: {
     type: String,
@@ -46,7 +66,7 @@ const productSchema = new mongoose.Schema({
     required: true,
     default: 0
   } 
-});
+}, {timestamps: true});
 
 const Product = mongoose.model('Product', productSchema);
 
